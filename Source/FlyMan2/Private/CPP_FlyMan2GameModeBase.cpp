9de8325111b11b45740364f7d4fdcbb590c64c5c
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2024-08-13 22:23:46 +0900

    キャラクターを切り替え

--- Source/FlyMan2/Private/CPP_FlyMan2GameModeBase.cpp
+++ Source/FlyMan2/Private/CPP_FlyMan2GameModeBase.cpp
@@ -9,7 +9,8 @@ ACPP_FlyMan2GameModeBase::ACPP_FlyMan2GameModeBase(const FObjectInitializer& Obj
 {
 	//ブループリントのキャラクターを取得
 	//static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/FlyMan2_Character.FlyMan2_Character_C"));
-	static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/BP_ThirdPersonCharacter_Jiro.BP_ThirdPersonCharacter_Jiro_C"));
+	//static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/BP_ThirdPersonCharacter_Jiro.BP_ThirdPersonCharacter_Jiro_C"));
+	static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/ThirdPersonCharacter1.ThirdPersonCharacter1_C"));
 
 	//取得に成功したら中身を実行する
 	if (playerClass.Succeeded())
