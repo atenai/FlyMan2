a0bb44f44c813a5db28eccb8fa97e6f29665448d
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2024-08-16 02:24:33 +0900

    キャラクターモデルのアニメーションのリターゲティング

--- Source/FlyMan2/Private/CPP_FlyMan2GameModeBase.cpp
+++ Source/FlyMan2/Private/CPP_FlyMan2GameModeBase.cpp
@@ -8,9 +8,8 @@
 ACPP_FlyMan2GameModeBase::ACPP_FlyMan2GameModeBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
 	//ブループリントのキャラクターを取得
-	//static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/FlyMan2_Character.FlyMan2_Character_C"));
-	//static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/BP_ThirdPersonCharacter_Jiro.BP_ThirdPersonCharacter_Jiro_C"));
-	static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/ThirdPersonCharacter1.ThirdPersonCharacter1_C"));
+	//static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/BP_FlyMan2_Character1.BP_FlyMan2_Character1_C"));
+	static ConstructorHelpers::FClassFinder<APawn> playerClass(TEXT("/Game/FlyMan2/Character/BP_Boy01.BP_Boy01_C"));
 
 	//取得に成功したら中身を実行する
 	if (playerClass.Succeeded())
